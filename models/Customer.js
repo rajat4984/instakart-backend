60c3f14c8fd37443022102015a5468c7880426c3
Rajat Sharma <rajat.sharma@ens.enterprises>
2025-02-24 17:18:12 +0530

    cstomer crud completed

--- models/Customer.js
+++ models/Customer.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const customerSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
-  lastName: { type: String, required: false }, // Optional
-  mobileNumber: { type: String, required: true },
-  email: { type: String, required: false }, // Optional
+  lastName: { type: String },
+  mobileNumber: { type: String, required: true, unique: true }, // Unique
+  email: { type: String, unique: true}, // Unique but optional
   addressLine1: { type: String, required: true },
-  addressLine2: { type: String, required: false }, // Optional
-  pincode: { type: String, required: false }, // Optional
+  addressLine2: { type: String },
+  pincode: { type: String },
 });
 
 const Customer = mongoose.model("Customer", customerSchema);
