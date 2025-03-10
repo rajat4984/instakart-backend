db79d32ab22a7a586e3415c575f94bf2e41cad72
Ayush <ayush.yadav.100100@gmail.com>
2025-03-10 17:59:49 +0530

    product pagination

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -24,3 +24,4 @@ app.use("/api/v1/orders", orderRoutes);
 
 // Connect to DB
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+ 
