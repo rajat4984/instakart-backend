5d1553cf6fb9bbed3bb0463485ed830dd729e2a2
Rajat Sharma <rajat.sharma@ens.enterprises>
2025-03-30 00:12:04 +0530

    order apitoken

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,14 +5,15 @@ import {
   updateProduct,
   deleteProduct,
   getProductById,
-  searchProducts,  // ✅ Import the new controller
+  searchProducts, // ✅ Import the new controller
 } from "../controller/productController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", getProducts); // Get all products
-router.get('/search',searchProducts);
-// router.get("/:id", getProductById); // ✅ Get a single product
+router.post("/getAllProducts", getProducts); // Get all products
+router.get("/search", searchProducts);
+router.get("/:id", getProductById); // ✅ Get a single product
 router.post("/", createProduct); // Create product
 router.put("/:id", updateProduct); // Edit product
 router.delete("/:id", deleteProduct); // Delete product
