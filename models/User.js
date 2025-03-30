a0bf21be69f37acfbd6a4d297a78ebb2bbfe673f
Rajat Sharma <rajat.sharma@ens.enterprises>
2025-03-30 17:18:50 +0530

    profile page completed

--- models/User.js
+++ models/User.js
@@ -3,9 +3,14 @@ import { Schema, model } from "mongoose";
 const UserSchema = new Schema({
   fullName: { type: String, required: true },
   businessName: { type: String, required: true },
+  profilePicture: { type: String, default: "" },
   email: { type: String, unique: true, sparse: true },
   mobile: { type: String, unique: true, sparse: true },
   password: { type: String, required: true },
+  country: { type: String }, // Added country
+  city: { type: String }, // Added city
+  postalCode: { type: String }, // Added postalCode
+  taxId: { type: String }, // Added taxId
 });
 
 export default model("User", UserSchema);
